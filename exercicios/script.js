42d0f6b851774e0f1468b5d43827fa8545089072
perkles <marcelossp01@yahoo.com.br>
2022-05-25 21:18:16 -0300

    backup

---
+++ exercicios/script.js
@@ -0,0 +1,31 @@
+// tipos primitivos 
+
+// boolean - roda o index e clica em F12, na aba console e tem o resol tado falso
+var VOuF = false;
+console.log(typeof{VOuF}); 
+
+// exemplo com o numero
+var numeroQualquer = 1;
+console.log(typeof{numeroQualquer});
+
+// string 
+var nome = 'Marcelo';
+console.log(typeof{nome});
+
+var funcao = function() {}
+console.log(typeof{funcao});
+
+// como declarar
+var variavel = 'Marcelo';
+variavel = 'Marcos';
+console.log(variavel);
+
+// let é uma variavel de escopo local de bloco
+let variavel2 = 'Marcelo';
+console.log(variavel2);
+
+// variavel constante, de valor fixo, não pode ser alterada
+const constante = 'Marcelo';
+console.log(constante);
+
+
